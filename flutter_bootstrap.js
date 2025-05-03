3e204097962bcd02500dabccbafd25bcc749872b
Muhammad Sinan <ms4730435@gmail.com>
2025-05-03 19:31:30 +0530

    web frist commmit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "495426503"
+    serviceWorkerVersion: "3488383423"
   }
 });
