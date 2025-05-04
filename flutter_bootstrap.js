21b77c9e318b12397ac4b2ae307e42f4cf94b9f9
Muhammad Sinan <ms4730435@gmail.com>
2025-05-04 11:58:41 +0530

    complted otp screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1592069768"
+    serviceWorkerVersion: "425109090"
   }
 });
