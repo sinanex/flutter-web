3be4178eb22223929e186b4d20036b7d66c9e543
Muhammad Sinan <ms4730435@gmail.com>
2025-05-04 11:05:32 +0530

    web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3224471425"
+    serviceWorkerVersion: "1592069768"
   }
 });
